1077672d989f6c539c98ebfa2d1214db2d2e8c82
Guillermo Duque <g.duque@gmail.com>
2021-10-27 10:18:16 +0200

    Add service methods

--- src/main/java/com/assignment/app/service/ProductService.java
+++ src/main/java/com/assignment/app/service/ProductService.java
@@ -1,17 +1,67 @@
 package com.assignment.app.service;
 
+import com.assignment.app.repo.ProductRepo;
+
 import java.util.ArrayList;
 
 public class ProductService {
-    public static String sayHi () {
-        return "hi";
+
+    ProductRepo productRepo = new ProductRepo();
+
+    private String extractImportedStatus(String line) {
+        if(line.contains("imported")) {
+            return " imported ";
+        }
+        return "";
+    }
+
+    private String extractProductName(String line) {
+        return line
+                .replaceAll("\\d","") // Remove digits
+                .replace(" at ", "") // Remove "at"
+                .replace(" imported ", "") // Remove "imported"
+                .replace(".", ""); // Remove the dot
+    }
+
+    private String turnProductInputToOutput (String line) {
+
+        char numberOfProducts = line.charAt(0);
+        String productName = extractProductName(line);
+        double price = productRepo.getProductPrice(productName);
+        String imported = extractImportedStatus(line);
+
+        return "> " + numberOfProducts + imported + productName + ": " + price + "\n";
     }
 
     public String checkout(ArrayList<String> productList) {
-        return "Output";
+        StringBuilder outputString = new StringBuilder();
+        double totalPrice = 0;
+        double salesTaxes = 0;
+
+        for (String product : productList) {
+           outputString.append(turnProductInputToOutput(product));
+           totalPrice += productRepo.getProductPrice(extractProductName(product));
+           if(extractImportedStatus(product).length() > 0) {
+               double taxAmount = productRepo.getProductPrice(extractProductName(product))/100*5;
+               salesTaxes += taxAmount;
+               totalPrice += taxAmount;
+           }
+
+        }
+
+        outputString
+                .append("> Sales Taxes: ")
+                .append(salesTaxes)
+                .append("\n")
+                .append("> Total: ")
+                .append(totalPrice)
+                .append("\n");
+
+
+        return outputString.toString();
     }
 
     public boolean checkIfExists(String selection) {
-        return false;
+        return productRepo.checkIfProductExists(selection);
     }
 }
