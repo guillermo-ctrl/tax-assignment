0ba67fb62aad83f67d091d9f679bdd6e1ff63520
Guillermo Duque <g.duque@gmail.com>
2021-10-27 11:29:01 +0200

    Add product repo tests

---
+++ src/test/java/com/assignment/app/repo/ProductRepoTest.java
@@ -0,0 +1,50 @@
+package com.assignment.app.repo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepoTest {
+    ProductRepo productRepo = new ProductRepo();
+
+    @Test
+    void getProductPrice() {
+
+        // Given
+        String product = "chocolate bar";
+
+        // When
+        double price = productRepo.getProductPrice(product);
+
+        // Then
+        assertEquals(12.49, price);
+
+    }
+
+    @Test
+    void productCheckDoesNotExist() {
+
+        // Given
+        String product = "chocolate chips";
+
+        // When
+        boolean check = productRepo.checkIfProductExists(product);
+
+        // Then
+        assertEquals(check,false);
+
+    }
+
+    @Test
+    void productCheckDoesExist() {
+        // Given
+        String product = "chocolate bar";
+
+        // When
+        boolean check = productRepo.checkIfProductExists(product);
+
+        // Then
+        assertEquals(check,true);
+    }
+
+}
